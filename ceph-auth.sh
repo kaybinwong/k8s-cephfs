d9d917152373e18cbab7d381732e9fecb510e979
huangkaibin <huangkaibin@seedland.cc>
2018-09-06 11:38:52 +0800

    add secret resources

--- ceph-auth.sh
+++ ceph-auth.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+function createSecret(){
+cat > secret.yaml << EOF
+---
+apiVersion: v1
+data:
+  secret: $SECRECT
+kind: Secret
+metadata:
+  name: ceph-admin-secret
+  namespace: cephfs
+type: Opaque
+EOF
+}
+
+BASE_DIR=$(cd `dirname $0`; pwd)
+cd $BASE_DIR
 ceph auth caps client.k8s mon 'allow rwx' osd 'allow rw pool=k8s' mds 'allow rwp' && ceph auth get-key client.k8s > /tmp/secret
+SECRECT=$(cat /tmp/secret | base64)
+createSecret
--- k8s-apply.sh
+++ k8s-apply.sh
@@ -1,3 +1,3 @@
 BASE_DIR=$(cd `dirname $0`; pwd)
-
-kubectl apply -f ns.yaml && kubectl create secret generic ceph-admin-secret --from-file=/tmp/secret --namespace=cephfs && kubectl apply -f $BASE_DIR
+cd $BASE_DIR
+kubectl apply -f .
